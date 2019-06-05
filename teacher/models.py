e58d46f90d99fcd0ea7e4623ac03fac150bbac7d
mmaina8 <joyymuthoni8@gmail.com>
2019-06-05 18:19:31 +0300

    First commit

---
+++ teacher/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Teacher(models.Model):
+	first_name= models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	date_of_birth = models.DateField()
+	registration_no = models.CharField(max_length=50)
+	place_of_residence = models.CharField(max_length=50)
+	phone_number = models.CharField(max_length=50)
+	email = models.EmailField()
+	id_number = models.IntegerField()
+	profession = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.first_name + self.last_name
